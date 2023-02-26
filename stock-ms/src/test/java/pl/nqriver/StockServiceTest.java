7833b0dd0c8e2c684dfecc92b3f3109284d5559c
nqriver <nqriver@gmail.com>
2023-02-26 23:09:53 +0100

    Initial commit

---
+++ stock-ms/src/test/java/pl/nqriver/StockServiceTest.java
@@ -0,0 +1,19 @@
+package pl.nqriver;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+public class StockServiceTest {
+
+    @QuarkusTest
+    public class StockServiceAppTests {
+
+        @Test
+        void startup() {
+
+        }
+    }
+
+
+}
